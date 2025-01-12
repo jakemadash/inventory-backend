c89bcf88a47491018dc2cdd614b90151fa755c90
jakemadash <jakemaddenashton@gmail.com>
2025-01-12 15:11:59 -0700

    chore: add cors package

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const createDynamicRouter = require("./entityRouter");
 const app = express();
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const entities = ["artists", "albums", "genres", "years"];
 entities.forEach((entity) => {
