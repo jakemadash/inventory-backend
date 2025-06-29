1870d211357a21af691c0b488f1432614b5b6374
jakemadash <jakemaddenashton@gmail.com>
2025-06-29 11:59:02 -0700

    fix: filter out null genre values and send back empty array by default

--- db/artists.js
+++ db/artists.js
@@ -3,13 +3,14 @@ const pool = require("./pool");
 const artistsDb = {
   getAll: async () => {
     const query = `SELECT 
-      a.artist,
-      json_agg(g.genre ORDER BY g.genre) AS genres
+    a.artist,
+    COALESCE(json_agg(g.genre ORDER BY g.genre) FILTER (WHERE g.genre IS NOT NULL), '[]') AS genres
     FROM artists a
     LEFT JOIN artist_genres ag ON a.artist_id = ag.artist_id
     LEFT JOIN genres g ON ag.genre_id = g.genre_id
     GROUP BY a.artist
-    ORDER BY a.artist;`;
+    ORDER BY a.artist;
+    `;
 
     const { rows } = await pool.query(query);
     return rows;
