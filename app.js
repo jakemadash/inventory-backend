1649f803cb91f66d5db96ea2a171a44610deb272
jakemadash <jakemaddenashton@gmail.com>
2025-07-12 12:10:18 -0700

    chore: remove unused years files and schemas doc

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const artistsRouter = require("./routes/artists");
 const albumsRouter = require("./routes/albums");
 const genresRouter = require("./routes/genres");
-const yearsRouter = require("./routes/years");
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(cors());
 app.use("/artists", artistsRouter);
 app.use("/albums", albumsRouter);
 app.use("/genres", genresRouter);
-app.use("/years", yearsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
